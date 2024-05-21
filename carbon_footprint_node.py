b9858c2564f212488df45e2096b5b9a69d497cde
JesusPoderoso <jesuspoderoso@eprosima.com>
2024-05-21 11:27:52 +0200

    Refs #21066: Introduce dummy Carbon tracker impl
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- carbon_footprint_node.py
+++ carbon_footprint_node.py
@@ -14,6 +14,9 @@
 """SustainML Carbon Footprint Node Implementation."""
 
 from sustainml_py.nodes.CarbonFootprintNode import CarbonFootprintNode
+from carbontracker.tracker import CarbonTracker
+from carbontracker import parser
+import time
 
 # Manage signaling
 import signal
@@ -34,10 +37,32 @@ def signal_handler(sig, frame):
 # Inputs: ml_model, user_input, hw
 # Outputs: node_status, co2
 def task_callback(ml_model, user_input, hw, node_status, co2):
+    log_directory = "/tmp/logs/carbontracker"
+    # Define CarbonTracker
+    tracker = CarbonTracker(log_dir=log_directory, epochs=1)
+    # Start measuring
+    tracker.epoch_start()
+    # Execute the training task
+    # ...
+    time.sleep(5)   # 5 seconds sleep as training
+    # Stop measuring
+    tracker.epoch_end()
+    tracker.stop()
 
-    # Callback implementation here
+    # Retrieve carbon information
+    logs = parser.parse_all_logs(log_dir=log_directory)
+    first_log = logs[0]
+    energy = first_log['pred']['energy (kWh)']
+    carbon = first_log['pred']['co2eq (g)']
+    intensity = 0
+    if (energy > 0):
+        intensity = carbon/energy
 
-    co2.carbon_intensity(4)
+    # Update output
+    co2.carbon_footprint((carbon/1000))     # kgCO2e
+    co2.energy_consumption((energy*1000))   # Wh
+    co2.carbon_intensity(intensity)         # gCO2/kWh
+
 
 # Main workflow routine
 def run():
