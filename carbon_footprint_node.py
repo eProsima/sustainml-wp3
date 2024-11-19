c5247a980296b0e66c4438987134561c5740f73f
Mario Dominguez <mariodominguez@eprosima.com>
2024-11-19 11:10:04 +0100

    Handle exceptions in carbontracker
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- carbon_footprint_node.py
+++ carbon_footprint_node.py
@@ -42,24 +42,37 @@ def task_callback(ml_model, user_input, hw, node_status, co2):
     energy_consump = hw.power_consumption()*default_time    # W * h = Wh
     log_directory = "/tmp/logs/carbontracker"               # temp log dir for reading carbon data results
 
-    # Define CarbonTracker
-    tracker = CarbonTracker(log_dir=log_directory, epochs=1)
-    # Start measuring
-    tracker.epoch_start()
-    # Execute the training task
-    # ...
-    time.sleep(2)   # 2 seconds sleep as training (temporal approach)
-    # Stop measuring
-    tracker.epoch_end()
-    tracker.stop()
+    # Default values
+    carbon = 1.0
+    intensity = 1.0
+    energy_consump = 1.0
 
-    # Retrieve carbon information
-    logs = parser.parse_all_logs(log_dir=log_directory)
-    first_log = logs[0]
-    carbon = first_log['pred']['co2eq (g)']
-    intensity = 0.0
-    if energy_consump > 0:
-        intensity = carbon/energy_consump
+    # CarbonTracker only supports Intel CPUs with the RAPL interface and NVIDIA GPUs.
+    # Catch the exception, warn and populate output with default values
+    try:
+        # Define CarbonTracker
+        tracker = CarbonTracker(log_dir=log_directory, epochs=1)
+         # Start measuring
+        tracker.epoch_start()
+        # Execute the training task
+        # ...
+        time.sleep(2)   # 2 seconds sleep as training (temporal approach)
+        # Stop measuring
+        tracker.epoch_end()
+        tracker.stop()
+
+        # Retrieve carbon information
+        logs = parser.parse_all_logs(log_dir=log_directory)
+        first_log = logs[0]
+        carbon = first_log['pred']['co2eq (g)']
+        intensity = 0.0
+        if energy_consump > 0.01:
+            intensity = carbon/energy_consump
+        else:
+            print("Energy consumption is too low. Carbon intensity cannot be calculated.")
+
+    except Exception as e:
+        print("CarbonTracker is not supported in this system. Default values will be used.")
 
     # populate carbon footprint information
     co2.carbon_footprint(carbon)
